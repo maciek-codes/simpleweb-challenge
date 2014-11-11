5139efc3502dff05c79e70e04163b09643f64c0a
Derek Bekoe <derek@derekbekoe.co.uk>
2014-11-11 20:44:37 +0000

    Working Tok

--- opentok-android-sdk-2.3.1/samples/OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
+++ opentok-android-sdk-2.3.1/samples/OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
@@ -2,18 +2,18 @@ package com.opentok.android.demo.config;
 
 public class OpenTokConfig {
 	
-	// *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+//*** Fill the following variables using your own Project info from the OpenTok dashboard  ***
 	// ***                      https://dashboard.tokbox.com/projects                           ***
 	// Replace with a generated Session ID
-	public static final String SESSION_ID = "";
+	public static final String SESSION_ID = "2_MX40NTA4NDE2Mn5-MTQxNTczMjQxNzcxNn5Wc1pSUUl6YWMrMVVERElvbCt6NE1TUE5-fg";
 	// Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-	public static final String TOKEN = ""; 
+	public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTA4NDE2MiZzaWc9YWUxZjRkZGM2OGM2YzA3NWE2M2ZjZWYzY2Q5MzVlMmEyNTMwNzNiMzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UQTROREUyTW41LU1UUXhOVGN6TWpReE56Y3hObjVXYzFwU1VVbDZZV01yTVZWRVJFbHZiQ3Q2TkUxVFVFNS1mZyZjcmVhdGVfdGltZT0xNDE1NzMyNjM5Jm5vbmNlPTAuNDMzMDI5MzM4NDk2MjcwNw=="; 
 	// Replace with your OpenTok API key
-	public static final String API_KEY= "";
+	public static final String API_KEY= "45084162";
 	
 	// Subscribe to a stream published by this client. Set to false to subscribe
-    // to other clients' streams only.
-    public static final boolean SUBSCRIBE_TO_SELF = true;
+   // to other clients' streams only.
+   public static final boolean SUBSCRIBE_TO_SELF = false;
     
    
 }
